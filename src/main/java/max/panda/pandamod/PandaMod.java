66387f2a29de81854ed79f5428c94cf686efc8eb
Max <rodriguezguell.max@gmail.com>
2023-08-21 12:20:16 +0200

    added ModItemGroups

--- src/main/java/max/panda/pandamod/PandaMod.java
+++ src/main/java/max/panda/pandamod/PandaMod.java
@@ -1,5 +1,6 @@
 package max.panda.pandamod;
 
+import max.panda.pandamod.item.ModItemGroups;
 import max.panda.pandamod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -14,5 +15,6 @@ public class PandaMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModItemGroups.RegisterModItemGroups();
 	}
 }
---
+++ src/main/java/max/panda/pandamod/item/ModItemGroups.java
@@ -0,0 +1,23 @@
+package max.panda.pandamod.item;
+
+import max.panda.pandamod.PandaMod;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup RUBY_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(PandaMod.MOD_ID, "ruby"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.ruby"))
+                    .icon(() -> new ItemStack(ModItems.RUBY)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.RUBY);
+                    }).build());
+
+    public static void RegisterModItemGroups() {
+        PandaMod.LOGGER.info("Registering ModItemGroups for " + PandaMod.MOD_ID);
+    }
+}
