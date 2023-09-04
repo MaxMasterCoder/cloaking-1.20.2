a07cc0968ec01776c23cb46d1ed7a086c20840e0
Max <rodriguezguell.max@gmail.com>
2023-09-04 18:03:54 +0200

    small changes

--- src/main/java/max/panda/cloaking/mixin/ExampleMixin.java
+++ src/main/java/max/panda/cloaking/mixin/ExampleMixin.java
@@ -1,5 +1,6 @@
 package max.panda.cloaking.mixin;
 
+import max.panda.cloaking.CloakingMod;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
